591eef039b5f835cac8cdd7825478b5e7cb58a58
Andrey Takhtamirov <andrey1229@icloud.com>
2021-10-11 13:38:43 -0400

    Destroy clouds upon movement outside of the world
    A new cloud will spawn when an old one has been destroyed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import Utils from "./utils";
 import Cloud from "./cloud";
 import Rain from "./rain";
 import World from "./world";
+import {UnrealBloomPass} from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+import {RenderPass} from "three/examples/jsm/postprocessing/RenderPass";
+import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import {ShaderPass} from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import {GUI} from "three/examples/jsm/libs/dat.gui.module";
 
 class App extends Component {
     componentDidMount() {
@@ -32,6 +37,7 @@ var scene;
 var worldReference;
 var camera;
 var renderer;
+var composer;
 
 function initializeWorld() {
     const worldSidesCount = 1;
@@ -41,21 +47,26 @@ function initializeWorld() {
     const maxNumOfForests = 4;
     const treesPerForest = 15;
 
-    const cloudRandomMovementX = Math.random() * (0.01 + 0.01) - 0.01;
-    const cloudRandomMovementZ = Math.random() * (0.01 + 0.01) - 0.01;
+    const cloudRandomMovementX = Utils.randomNumber(-0.001, 0.001);
+    const cloudRandomMovementY = Utils.randomNumber(-0.001, 0.001);
+    const cloudRandomMovementZ = Utils.randomNumber(-0.001, 0.001);
+
+    const cloudParticleMovementSpeed = 0.001;
 
     let worlds = [];
     let clouds = [];
-    let cloudParticles = [];
+    let cloudParticles = Utils.create2dArray(cloudsCount);
     let rainDrops = [];
 
-    let initialCloudParticleLocation = Utils.create2dArray(3);
+    let initialCloudParticleLocation = Utils.create2dArray(cloudsCount);
+    let cloudParticleMovement = Utils.create2dArray(cloudsCount);
+    let lastMovement = Utils.create2dArray(cloudsCount);
     let moveCounter = 0;
     let moveForward = true;
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color('black');
-    scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+    scene.background = new THREE.Color("rgb(236,222,136)");
+    scene.add(new THREE.AmbientLight(0xffffff, 0.6));
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100000);
     renderer = new THREE.WebGLRenderer({antialias: true});
 
@@ -65,15 +76,37 @@ function initializeWorld() {
 
     const controls = new OrbitControls(camera, renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setPixelRatio(window.devicePixelRatio*0.90);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     rootElement.appendChild(renderer.domElement);
+
+    const params = {
+        exposure: 1,
+        bloomStrength: 1.5,
+        bloomThreshold: 0,
+        bloomRadius: 1
+    };
+
+    const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+    bloomPass.threshold = params.bloomThreshold;
+    bloomPass.strength = params.bloomStrength;
+    bloomPass.radius = params.bloomRadius;
+    composer = new EffectComposer(renderer);
+    const renderScene = new RenderPass(scene, camera);
+    composer.addPass(renderScene);
+    composer.addPass(bloomPass)
 
     let star1 = new Star(1800, 1000, 1000, 500, 2.2);
     let star2 = new Star(-800, -1900, 100, 200, 2);
     scene.add(star1.light);
     scene.add(star2.light);
+
+    //light helper
+    // const helper = new THREE.CameraHelper(star1.light.shadow.camera);
+    // scene.add(helper);
+    // const helper2 = new THREE.CameraHelper(star2.light.shadow.camera);
+    // scene.add(helper2);
 
     // Don't show stars (too many objects cause low frame rates)
     // scene.add(star1.mesh);
@@ -117,7 +150,7 @@ function initializeWorld() {
         }
 
         for (let j = 0; j < cloudsCount; j++) {
-            clouds[i + j] = new Cloud(cloudParticles, worldReference);
+            clouds[i + j] = new Cloud(cloudParticles[j], worldReference);
             worlds[i].add(clouds[i + j]);
             // for (let k = 0; k < rainDropsPerCloud; k++) {
             //     rainDrops[k] = new Rain().mesh;
@@ -126,18 +159,28 @@ function initializeWorld() {
         }
     }
 
-    for (let i = 0; i < cloudParticles.length; i++) {
-        initialCloudParticleLocation[0][i] = cloudParticles[i].position.x;
-        initialCloudParticleLocation[1][i] = cloudParticles[i].position.y;
-        initialCloudParticleLocation[2][i] = cloudParticles[i].position.z;
+    for (let i = 0; i < cloudsCount; i++) {
+        for (let j = 0; j < cloudParticles[i].length; j++) {
+            initialCloudParticleLocation[j][0] = cloudParticles[i][j].position.x;
+            initialCloudParticleLocation[j][1] = cloudParticles[i][j].position.y;
+            initialCloudParticleLocation[j][2] = cloudParticles[i][j].position.z;
+        }
     }
-
-    camera.position.set(120, 120, 200);
+    camera.position.set(40, 25, 50); // Camera position for small earth (50x50)
+    //camera.position.set(120, 120, 200); // Camera position for medium earth (100x100)
     // camera.position.set(5000, 5000, 5000); // Camera position for huge earth
     controls.update();
 
+    for (let i = 0; i < cloudsCount; i++) {
+        cloudParticleMovement[i][0] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+        cloudParticleMovement[i][1] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+        cloudParticleMovement[i][2] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+    }
+
     let animate = function () {
         requestAnimationFrame(animate);
+        composer.render();
+
         // stats.update();
 
         // ---------------- WORLD MOVEMENT ---------------- //
@@ -151,37 +194,51 @@ function initializeWorld() {
 
         if (clouds.length > 0) {
             // ---------------- WHOLE CLOUD MOVEMENT ---------------- //
-            // for (let i = 0; i < clouds.length; i++) {
-            //     clouds[i].position.set(clouds[i].position.x + cloudRandomMovementX, clouds[i].position.y, clouds[i].position.z + cloudRandomMovementZ);
-            //
-            //     // for (let j = 0; j < rainDrops.length; j++) {
-            //     //     const rainDropSpeed = Math.random() * (0.1 + 0.04) - 0.04;
-            //     //     rainDrops[j].rainDrop.position.set(rainDrops[j].rainDrop.position.x, rainDrops[j].rainDrop.position.y - rainDropSpeed, rainDrops[j].rainDrop.position.z);
-            //     // }
-            // }
+            for (let i = 0; i < clouds.length; i++) {
+                clouds[i].translateX(cloudRandomMovementX);
+                clouds[i].translateY(cloudRandomMovementY);
+                clouds[i].translateZ(cloudRandomMovementZ);
 
-            // ---------------- CLOUD PARTICLE MOVEMENT ---------------- //
-            // for (let i = 0; i < cloudParticles.length; i++) {
-            //     // let randomMovementX = Utils.randomNumber(-0.05, 0.05);
-            //     // let randomMovementY = Utils.randomNumber(-0.05, 0.05);
-            //     // let randomMovementZ = Utils.randomNumber(-0.05, 0.05);
-            //     let position = cloudParticles[i].position;
-            //     let movement = 0.001;
-            //
-            //     if (!moveForward) {
-            //         cloudParticles[i].position.set(position.x - movement, position.y - movement, position.z - movement);
-            //     } else if (moveForward) {
-            //         cloudParticles[i].position.set(position.x + movement, position.y + movement, position.z + movement);
-            //     }
-            //
-            //     if (moveCounter % 5000 === 0){
-            //         moveForward = false;
-            //         //cloudParticles[i].position.set(initialCloudParticleLocation[0][i], initialCloudParticleLocation[1][i], initialCloudParticleLocation[2][i]);
-            //     } else if (moveCounter % 2500 === 0) {
-            //         moveForward = true;
-            //     }
-            //     moveCounter++;
-            // }
+                if (clouds[i].position.x > worlds[0].geometry.parameters.width / 2 ||
+                    clouds[i].position.x < -worlds[0].geometry.parameters.width / 2 ||
+                    clouds[i].position.y > 20 ||
+                    clouds[i].position.y < 0 ||
+                    clouds[i].position.z > worlds[0].geometry.parameters.depth / 2 ||
+                    clouds[i].position.z < -worlds[0].geometry.parameters.depth / 2) {
+
+                    worlds[0].remove(clouds[i]);
+                    clouds[i].geometry.dispose();
+                    clouds[i].material.dispose();
+                    clouds[i] = new Cloud(cloudParticles, worldReference);
+                    worlds[0].add(clouds[i]);
+                    // clouds.splice(i, 1); // Remove cloud from array
+                }
+            }
+
+            // ---------------- CLOUD PARTICLE MOVEMENT ---------------- //\
+            for (let i = 0; i < clouds.length; i++) {
+                for (let j = 0; j < cloudParticles[i].length; j++) {
+                    if (!moveForward) {
+                        cloudParticles[i][j].translateX(-cloudParticleMovement[i][0]);
+                        cloudParticles[i][j].translateY(-cloudParticleMovement[i][1]);
+                        cloudParticles[i][j].translateZ(-cloudParticleMovement[i][2]);
+                    } else if (moveForward) {
+                        cloudParticles[i][j].translateX(cloudParticleMovement[i][0]);
+                        cloudParticles[i][j].translateY(cloudParticleMovement[i][1]);
+                        cloudParticles[i][j].translateZ(cloudParticleMovement[i][2]);
+                    }
+
+                    if (moveCounter % 5000 === 0) {
+                        moveForward = false;
+                    } else if (moveCounter % 2500 === 0) {
+                        moveForward = true;
+                        cloudParticleMovement[i][0] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+                        cloudParticleMovement[i][1] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+                        cloudParticleMovement[i][2] = Utils.randomNumber(-cloudParticleMovementSpeed, cloudParticleMovementSpeed);
+                    }
+                    moveCounter++;
+                }
+            }
 
             // ---------------- RAIN DROP MOVEMENT ---------------- //
             // for (let i = 0; i < rainDrops.length; i++) {
@@ -206,4 +263,5 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    composer.setSize(window.innerWidth, window.innerHeight);
 }
