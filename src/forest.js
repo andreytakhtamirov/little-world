6862fd1fdc4ce42f0b2aa24a43319d0f50957d05
Andrey Takhtamirov <andrey1229@icloud.com>
2021-10-11 13:38:08 -0400

    Add variable leaf colours

--- src/forest.js
+++ src/forest.js
@@ -30,7 +30,7 @@ class Tree {
         const stemHeight = randDimension;
         const stemDepth = stemWidth;    // Tree trunk will be square-based
 
-        let stemMaterial = new THREE.MeshStandardMaterial({color: "rgb(62,39,25)"});
+        let stemMaterial = new THREE.MeshStandardMaterial({color: "rgb(62,35,25)"});
         let stemGeometry = new THREE.BoxGeometry(stemWidth, stemHeight, stemDepth);
 
         let stemLineMaterial = new THREE.LineBasicMaterial({color: "rgb(43,28,28)"});
@@ -59,6 +59,13 @@ class Leaf {
         // Leaf dimensions (cube)
         const leafWidthHeightDepth = Utils.randomNumber(2, 2.5) * stemMesh.geometry.parameters.width;
 
+        // Leaf colours
+        const leafColours =
+            ["rgb(24,65,15)", "rgb(35,80,15)",
+                "rgb(19,42,16)", "rgb(14,61,12)",
+                "rgb(26,77,5)", "rgb(31,52,17)",
+                "rgb(19,45,6)", "rgb(23,61,12)"];
+
         // Leaf positions
         const treeHeight = stemMesh.geometry.parameters.height;
         const minLeafPosY = treeHeight / 2 - leafWidthHeightDepth / 2;
@@ -66,10 +73,12 @@ class Leaf {
         const randomLeafPositionY = Math.random() * (treeHeight / 2 - minLeafPosY) + minLeafPosY;
         const randomLeafPositionZ = Math.random() * (leafWidthHeightDepth / 2 + leafWidthHeightDepth / 4) - leafWidthHeightDepth / 4;
 
-        let leafMaterial = new THREE.MeshStandardMaterial({color: "rgb(79, 121, 59)"});
+        let randomColour = new THREE.Color(leafColours[(Utils.randomInteger(0, leafColours.length - 1))]);
+        let leafMaterial = new THREE.MeshLambertMaterial({color: randomColour});
+
         let leafGeometry = new THREE.BoxGeometry(leafWidthHeightDepth, leafWidthHeightDepth, leafWidthHeightDepth);
 
-        let leafLineMaterial = new THREE.LineBasicMaterial({color: "rgb(35, 80, 22)"});
+        let leafLineMaterial = new THREE.LineBasicMaterial({color: "rgb(11,35,4)"});
         let leafLine = new THREE.LineSegments(new THREE.EdgesGeometry(leafGeometry), leafLineMaterial);
 
         this.mesh = new THREE.Mesh(leafGeometry, leafMaterial);
