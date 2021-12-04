0264602f0fce04b17c2016424d7efe4b6c0b90bf
Andrey Takhtamirov <andrey1229@icloud.com>
2021-12-04 00:31:04 -0500

    Revert high-performance render mode

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function initializeWorld() {
     scene.add(new THREE.AmbientLight("rgb(255,255,255)", 0.3));
     scene.add(weather.sceneAmbientLight2);
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, Constants.World.Width * Constants.World.Depth * Constants.World.SidesCount);
-    renderer = new THREE.WebGLRenderer({antialias: true, powerPreference: "high-performance"});
+    renderer = new THREE.WebGLRenderer({antialias: true});
     if (window.screen.width * window.devicePixelRatio > Constants.Page.ResolutionWidth) {
         Constants.Page.ResolutionRatio = Constants.Page.ResolutionWidth / (window.screen.width * window.devicePixelRatio);
     }
