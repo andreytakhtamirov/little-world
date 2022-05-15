960f66cab93c647bb7227f5853e94b93e93e3c55
Andrey Takhtamirov <andrey1229@icloud.com>
2022-05-15 19:35:13 -0400

    Fix tree position for custom world dimensions.

--- src/entities/world.js
+++ src/entities/world.js
@@ -4,22 +4,35 @@ import * as Constants from "../worldProperties/constants";
 import * as Colours from "../worldProperties/colours"
 
 export default class World {
-    constructor(weather) {
-        const worldWidth = Constants.World.Width;
-        const worldHeight = Constants.World.Height;
-        const worldDepth = Constants.World.Depth;
+    constructor(weather, width, height, depth, numForests, numClouds) {
+        this.numForests = numForests;
+        this.numClouds = numClouds;
+
+        if (width == null) {
+            width = Constants.World.Width;
+        }
+        if (height == null) {
+            height = Constants.World.Height;
+        }
+        if (depth == null) {
+            depth = Constants.World.Depth;
+        }
+        if (numForests == null) {
+            this.numForests = Utils.randomInteger(Constants.Forest.MinCount, Constants.Forest.MaxCount)
+        }
+        if (numClouds == null) {
+            this.numClouds = Utils.randomInteger(Constants.Cloud.MinCount, Constants.Cloud.MaxCount);
+        }
 
         let worldColour = Colours.World.Grass;
         if (weather.conditions.includes('snowy')) {
             worldColour = Colours.World.SnowyGrass;
         }
-        const worldGeometry = new THREE.BoxBufferGeometry(worldWidth, worldHeight, worldDepth);
+        const worldGeometry = new THREE.BoxBufferGeometry(width, height, depth);
         const grassMaterial = new THREE.MeshPhongMaterial({color: worldColour});
 
         this.mesh = new THREE.Mesh(worldGeometry, grassMaterial);
         this.mesh.receiveShadow = true;
-        this.numForests = Utils.randomInteger(Constants.Forest.MinCount, Constants.Forest.MaxCount)
-        this.numClouds = Utils.randomInteger(Constants.Cloud.MinCount, Constants.Cloud.MaxCount);
         this.forests = [];
         this.clouds = [];
         this.snow = [];
